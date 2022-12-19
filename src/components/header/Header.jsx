0d9ff8a2f0c15e9648c8522aa85d327f91551620
Denis Sirenko <sirenko.denis86@gmail.com>
2022-12-19 20:40:20 +0200

    add header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,96 @@
-import React from 'react';
+import { useRef, useEffect } from 'react';
+
+import { Container } from 'reactstrap';
+import logo from '../../assets/images/res-logo.png';
+import { NavLink, Link } from 'react-router-dom';
+
+import '../../styles/header.css';
+
+const nav__links = [
+  {
+    display: 'Home',
+    path: '/home',
+  },
+  {
+    display: 'Foods',
+    path: '/foods',
+  },
+  {
+    display: 'Cart',
+    path: '/cart',
+  },
+  {
+    display: 'Contact',
+    path: '/contact',
+  },
+];
 
 export const Header = () => {
-  return <div>Header</div>;
+  const menuRef = useRef(null);
+  const headerRef = useRef(null);
+
+  const toggleMenu = (e) => {
+    menuRef.current.classList.toggle('show__menu');
+  };
+
+  const toggleCart = () => {
+    console.log('toggleCart');
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        headerRef.current.classList.add('header__shrink');
+      } else {
+        headerRef.current.classList.remove('header__shrink');
+      }
+    });
+
+    return () => window.removeEventListener('scroll');
+  }, []);
+
+  return (
+    <header className="header">
+      <Container>
+        <div className="nav__wrapper d-flex align-items-center justify-content-between">
+          <div className="logo">
+            <img src={logo} alt="logo" />
+            <h5>Tasty Treat</h5>
+          </div>
+
+          <div className="navigation" ref={menuRef} onClick={(e) => toggleMenu(e)}>
+            <div className="menu d-flex align-items-center gap-5">
+              {nav__links.map((item, index) => (
+                <NavLink
+                  to={item.path}
+                  key={index}
+                  className={(navClass) => (navClass.isActive ? 'active__menu' : '')}>
+                  {item.display}
+                </NavLink>
+              ))}
+            </div>
+          </div>
+
+          <div className="nav__right d-flex align-items-center gap-4">
+            <span className="cart__icon" onClick={toggleCart}>
+              <i className="ri-shopping-basket-line"></i>
+              <span className="cart__badge">5</span>
+            </span>
+
+            <span className="user">
+              <Link to="/login">
+                <i className="ri-user-line"></i>
+              </Link>
+            </span>
+
+            <span className="mobile__menu" onClick={(e) => toggleMenu(e)}>
+              <i className="ri-menu-line"></i>
+            </span>
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
 };
 
 export default Header;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import App from "./App";
-import "bootstrap/dist/css/bootstrap.css";
-import "remixicon/fonts/remixicon.css";
-import "./index.css";
+import App from './App';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'remixicon/fonts/remixicon.css';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
